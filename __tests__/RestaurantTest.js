03f98233da649b1e8286547626ca877d8043b23e
jijiseong <jiseong5036@gmail.com>
2023-11-14 21:44:08 +0900

    test: EventCalculator 테스트 추가
    - 각종 할인 계산 테스트

--- __tests__/RestaurantTest.js
+++ __tests__/RestaurantTest.js
@@ -19,4 +19,18 @@ describe('Restaurant 객체 테스트', () => {
     // then
     expect(hasMenu).toBe(true);
   });
+
+  test('메뉴 가격을 계산할 수 있다.', () => {
+    // given
+    const menus = {
+      해산물파스타: 1,
+      양송이수프: 1,
+    };
+
+    // when
+    const totalPrice = restaurant.calculateTotalPrice(menus);
+
+    // then
+    expect(totalPrice).toBe(MENU['해산물파스타'] + MENU['양송이수프']);
+  });
 });
